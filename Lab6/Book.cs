1dd2919ca5134d6ef35bfc81462181bc74b197ba
margoboiko <56150348+margoboiko@users.noreply.github.com>
2020-05-24 16:50:03 +0300

    Add files via upload

---
+++ Lab6/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab6
+{
+    public class Book : IHasName
+    {
+        public string Name { get; }
+
+        public Book(string name)
+        {
+            Name = name;
+        }
+    }
+}
